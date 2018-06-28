02b337fafd284329adc51206c3fef515f9fbd08e
Dirk Marciniak <dirk.marciniak@mail.de>
2018-06-28 10:34:02 +0200

    Kommentare zugefügt debugtexte zugefügt getter zugefügt

--- libsrc/xmlparser/wsaudiospcontrols.cpp
+++ libsrc/xmlparser/wsaudiospcontrols.cpp
@@ -2,11 +2,16 @@
 
 namespace bose_soundtoch_lib
 {
+  /**
+   * @brief WsAudioSpControls::WsAudioSpControls
+   * @param xmlreader
+   * @param parent
+   */
   WsAudioSpControls::WsAudioSpControls( QXmlStreamReader *xmlreader, QObject *parent ) : IResponseObject( xmlreader, parent )
   {
     Q_ASSERT( reader->isStartElement() && reader->name() == QLatin1String( "audiospcontrols" ) );
     resultType = ResultobjectType::U_AUDIO_SP_CONTROLS_UNSUPPORTED;
-    qDebug() << "(unsupported)...";
+    qDebug() << "updated (unsupported)...";
     controls = reader->readElementText();
     while ( reader->readNextStartElement() && !reader->hasError() )
     {
@@ -14,9 +19,21 @@ namespace bose_soundtoch_lib
     }
   }
 
+  /**
+   * @brief WsAudioSpControls::~WsAudioSpControls
+   */
   WsAudioSpControls::~WsAudioSpControls()
   {
     qDebug() << "...";
+  }
+
+  //
+  // GETTER
+  //
+
+  QString WsAudioSpControls::getControls() const
+  {
+    return controls;
   }
 
 }  // namespace bose_soundtoch_lib
--- libsrc/xmlparser/wsaudiospcontrols.hpp
+++ libsrc/xmlparser/wsaudiospcontrols.hpp
@@ -14,11 +14,15 @@ namespace bose_soundtoch_lib
   class WsAudioSpControls : public IResponseObject
   {
     private:
-    QString controls;
+    QString controls;  //! Controls...
 
     public:
     explicit WsAudioSpControls( QXmlStreamReader *xmlreader, QObject *parent );
     ~WsAudioSpControls() override;
+    //
+    // GETTER
+    //
+    QString getControls() const;
   };
 }  // namespace bose_soundtoch_lib
 
