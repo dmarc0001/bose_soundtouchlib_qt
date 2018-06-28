a3e0b25dc57af7decf04d849016aed026493fc0e
Dirk Marciniak <dirk.marciniak@mail.de>
2018-06-28 10:00:57 +0200

    Kommentare zugefügt

--- libsrc/xmlparser/wsuseractivityupdated.cpp
+++ libsrc/xmlparser/wsuseractivityupdated.cpp
@@ -2,15 +2,23 @@
 
 namespace bose_soundtoch_lib
 {
+  /**
+   * @brief WsUserActivityUpdated::WsUserActivityUpdated
+   * @param xmlreader
+   * @param parent
+   */
   WsUserActivityUpdated::WsUserActivityUpdated( QXmlStreamReader *xmlreader, QObject *parent ) : IResponseObject( xmlreader, parent )
   {
     Q_ASSERT( reader->isStartElement() && reader->name() == QLatin1String( "userActivityUpdate" ) );
-    qDebug() << "(unsupported)...";
+    qDebug() << "updated (unsupported)...";
     resultType = ResultobjectType::U_USER_ACTIVITY_UPDATED_UNSUPPORTED;
     // als root tag...
     deviceId = getAttibute( reader, QLatin1String( "deviceId" ) );
   }
 
+  /**
+   * @brief WsUserActivityUpdated::~WsUserActivityUpdated
+   */
   WsUserActivityUpdated::~WsUserActivityUpdated()
   {
     qDebug() << "...";
