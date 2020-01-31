868b3d3b7508eaeb1c5bed8476fdc92d04a429e5
Dirk Marciniak <dirk.marciniak@mail.de>
2020-01-31 23:00:33 +0100

    umstellung zu cmake bei library, test folgt

---
+++ libsoundtouch/Libsoundtouch.cpp
@@ -0,0 +1,5 @@
+#include "Libsoundtouch.hpp"
+
+Libsoundtouch::Libsoundtouch()
+{
+}
---
+++ libsoundtouch/Libsoundtouch.hpp
@@ -0,0 +1,12 @@
+#ifndef LIBSOUNDTOUCH_HPP
+#define LIBSOUNDTOUCH_HPP
+
+#include "libsoundtouch_global.hpp"
+
+class LIBSOUNDTOUCH_EXPORT Libsoundtouch
+{
+  public:
+  Libsoundtouch();
+};
+
+#endif // LIBSOUNDTOUCH_HPP
