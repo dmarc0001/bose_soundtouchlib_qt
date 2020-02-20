7921726fbdd88cf42ee60d4adee8efbf3820c30a
Dirk Marciniak <dirk.marciniak@mail.de>
2020-02-20 20:57:14 +0100

    alarm handling object

---
+++ alert_daemon/daemon/bosesoundalert.cpp
@@ -0,0 +1,6 @@
+#include "bosesoundalert.hpp"
+
+bosesoundalert::bosesoundalert()
+{
+
+}
---
+++ alert_daemon/daemon/bosesoundalert.hpp
@@ -0,0 +1,13 @@
+#ifndef BOSESOUNDALERT_HPP
+#define BOSESOUNDALERT_HPP
+
+#include <QObject>
+
+class bosesoundalert : public Thread
+{
+  Q_OBJECT
+  public:
+  bosesoundalert();
+};
+
+#endif // BOSESOUNDALERT_HPP
