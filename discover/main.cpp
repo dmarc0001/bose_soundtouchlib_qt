4a21eae0dfd4e4650a12107e5900397ceb407e0e
Dirk Marciniak <dirk.marciniak@mail.de>
2020-02-27 20:27:24 +0100

    unnötig

--- discover/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "soundtouchdiscover.hpp"
-#include <QCoreApplication>
-
-int main(int argc, char *argv[]) {
-  QCoreApplication a(argc, argv);
-  SoundTouchDiscover discover;
-  discover.run();
-  return a.exec();
-}
